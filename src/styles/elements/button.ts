15082bcf9aee384a9b3e57b8b0736cffb120324e
Marshall Bowers <mbowers@gravic.com>
2017-04-12 12:24:28 -0400

    Add sample style object for Button

---
+++ src/styles/elements/button.ts
@@ -0,0 +1,55 @@
+const theme = {
+  verticalAlign: '',
+  background: '',
+  textColor: '',
+  fontFamily: '',
+  horizontalMargin: '',
+  verticalMargin: '',
+  verticalPadding: '',
+  horizontalPadding: '',
+  shadowOffset: '',
+  textTransform: '',
+  textShadow: '',
+  fontWeight: '',
+  lineHeight: '',
+  borderRadius: '',
+  boxShadow: '',
+  transition: '',
+  willChange: '',
+  tapColor: ''
+};
+
+const button = {
+  cursor: 'pointer',
+  display: 'inline-block',
+
+  minHeight: '1em',
+
+  outline: 'none',
+  border: 'none',
+  verticalAlign: theme.verticalAlign,
+  background: theme.background,
+  color: theme.textColor,
+
+  fontFamily: theme.fontFamily,
+
+  margin: `0em ${theme.horizontalMargin} ${theme.verticalMargin} 0em`,
+  padding: `${theme.verticalPadding} ${theme.horizontalPadding} ${theme.verticalPadding + theme.shadowOffset}`,
+
+  textTransform: theme.textTransform,
+  textShadow: theme.textShadow,
+  fontWeight: theme.fontWeight,
+  lineHeight: theme.lineHeight,
+  fontStyle: 'normal',
+  textAlign: 'center',
+  textDecoration: 'none',
+
+  borderRadius: theme.borderRadius,
+  boxShadow: theme.boxShadow,
+
+  userSelect: 'none',
+  transition: theme.transition,
+  willChange: theme.willChange,
+
+  WebkitTapHighlightColor: theme.tapColor
+};
