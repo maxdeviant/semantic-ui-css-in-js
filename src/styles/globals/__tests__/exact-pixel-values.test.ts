77ca531756e365863ee340a456145f88133fa325
Marshall Bowers <elliott.codes@gmail.com>
2017-04-14 00:29:32 -0400

    Add sanity check tests

--- src/styles/globals/__tests__/exact-pixel-values.test.ts
+++ src/styles/globals/__tests__/exact-pixel-values.test.ts
@@ -9,6 +9,10 @@ import {
 } from '../exact-pixel-values';
 
 describe('pixelsToEm', () => {
+  it('should sanity check', () => {
+    expect(pixelsToEm(24)).toBe(`${24 / emSize}em`);
+  });
+
   it('should convert pixels to em using the defined base size', () => {
     expect(pixelsToEm(24)).toBe(em(24, emSize));
     expect(pixelsToEm('24px')).toBe(em(24, emSize));
@@ -16,6 +20,10 @@ describe('pixelsToEm', () => {
 });
 
 describe('pixelsToRem', () => {
+  it('should sanity check', () => {
+    expect(pixelsToRem(24)).toBe(`${24 / emSize}rem`);
+  });
+
   it('should convert pixels to rem using the defined base size', () => {
     expect(pixelsToRem(24)).toBe(rem(24, emSize));
     expect(pixelsToRem('24px')).toBe(rem(24, emSize));
