f474c0b32440f5d59637575215690c6643c2fd2c
Marshall Bowers <elliott.codes@gmail.com>
2017-04-13 21:45:21 -0400

    Add more global styles

--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -4,7 +4,11 @@ import {
   rem,
   saturate
 } from 'polished';
-import { neutralColors } from './colors';
+import {
+  colors,
+  lightColors,
+  neutralColors
+} from './colors';
 
 // Fonts
 
@@ -39,10 +43,23 @@ export const absoluteBorderRadius = pixelsToRem(4);
 export const defaultBorderRadius = absoluteBorderRadius;
 
 // Brand Colors
-// TODO: Brand Colors
+
+export const primaryColor = colors.blue;
+export const secondaryColor = colors.black;
+
+export const lightPrimaryColor = lightColors.blue;
+export const lightSecondaryColor = lightColors.black;
 
 // Page Heading
-// TODO: Page Heading
+
+export const headerFontWeight = 'bold';
+export const headerLineHeight = em(18 / 14);
+
+export const h1 = rem(28 / 14);
+export const h2 = rem(24 / 14);
+export const h3 = rem(18 / 14);
+export const h4 = rem(15 / 14);
+export const h5 = rem(14 / 14);
 
 // Form Input
 
@@ -92,6 +109,77 @@ export const linkColor = '#4183c4';
 export const linkUnderline = 'none';
 export const linkHoverColor = darken(15, saturate(20, linkColor));
 export const linkHoverUnderline = linkUnderline;
+
+// Highlighted Text
+
+export const highlightBackground = '#cce2ff';
+export const highlightColor = textColor;
+
+export const inputHighlightBackground = 'rgba(100,100,100,.4)';
+export const inputHightlightColor = textColor;
+
+// Loader
+// TODO: Loader
+
+// Icons
+
+/**
+ * Maximum glyph width for icons.
+ */
+export const iconWidth = '1.18em';
+
+// Neutral Text
+
+export const darkTextColor = 'rgba(0,0,0,.85)';
+export const mutedTextColor = 'rgba(0,0,0,.6)';
+export const lightTextColor = 'rgba(0,0,0,.4)';
+
+export const unselectedTextColor = 'rgba(0,0,0,.4)';
+export const hoveredTextColor = 'rgba(0,0,0,.8)';
+export const pressedTextColor = 'rgba(0,0,0,.9)';
+export const selectedTextColor = 'rgba(0,0,0,.95)';
+export const disabledTextColor = 'rgba(0,0,0,.2)';
+
+export const invertedTextColor = 'rgba(255,255,255,.9)';
+export const invertedMutedTextColor = 'rgba(255,255,255,.8)';
+export const invertedLightTextColor = 'rgba(255,255,255,.7)';
+export const invertedUnselectedTextColor = 'rgba(255,255,255,.5)';
+export const invertedHoveredTextColor = 'rgba(255,255,255,1)';
+export const invertedPressedTextColor = 'rgba(255,255,255,1)';
+export const invertedSelectedTextColor = 'rgba(255,255,255,1)';
+export const invertedDisabledTextColor = 'rgba(255,255,255,.2)';
+
+// Brand Colors
+
+export const facebookColor = '#3b5998';
+export const twitterColor = '#55acee';
+export const googlePlusColor = '#dd4b39';
+export const linkedInColor = '#1f88be';
+export const youtubeColor = '#cc181e';
+export const pinterestColor = '#bd081c';
+export const vkColor = '#4d7198';
+export const instagramColor = '#49769c';
+
+// Borders
+
+export const circularRadius = '500rem';
+
+export const borderColor = 'rgba(34,36,38,.15)';
+export const strongBorderColor = 'rgba(34,36,38,.22)';
+export const internalBorderColor = 'rgba(34,36,38,.1)';
+export const selectedBorderColor = 'rgba(34,36,38,.35)';
+export const strongSelectedBorderColor = 'rgba(34,36,38,.5)';
+export const disabledBorderColor = 'rgba(34,36,38,.5)';
+
+export const solidInternalBorderColor = '#fafafa';
+export const solidBorderColor = '#d4d4d5';
+export const solidSelectedBorderColor = '#bcbdbd';
+
+export const whiteBorderColor = 'rgba(255,255,255,.1)';
+export const selectedWhiteBorderColor = 'rgba(255,255,255,.8)';
+
+export const solidWhiteBorderColor = '#555555';
+export const selectedSolidWhiteBorderColor = '#999999';
 
 // Exact Pixel Values
 
