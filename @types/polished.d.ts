66a1da50d4592756af9a0ad70ededb5853303963
Marshall Bowers <elliott.codes@gmail.com>
2017-04-13 21:31:30 -0400

    Add Polished typings

--- @types/polished.d.ts
+++ @types/polished.d.ts
@@ -1,1 +1,21 @@
-declare module 'polished';
+declare namespace Polished {
+
+  // Colors
+
+  function darken(amount: number, color: string): string;
+
+  function saturate(amount: number, color: string): string;
+
+  // Helpers
+
+  function em(pxval: string | number, base?: string | number): string;
+
+  function rem(pxval: string | number, base?: string | number): string;
+
+  function stripUnit(value: string): number | string;
+
+}
+
+declare module 'polished' {
+  export = Polished;
+}
